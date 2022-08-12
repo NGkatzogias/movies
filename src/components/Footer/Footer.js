fd6ce348bd359b1fa9a8f423e1bec746a6ad0d98
Nikos Gkatzogias <100298545+NGkatzogias@users.noreply.github.com>
2022-08-12 17:55:18 +0300

    added the movie database logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import './Footer.css'
 
+import tmdb from '../../images/tmdb-logo.svg'
+
 export default function Footer() {
   return (
-    <footer>All Right Reserved Nikos Gkatzogias 2022</footer>
+    <footer>
+      <div className="tmdb">
+        <img className='tmdb' src={tmdb} alt="the movie database logo"/>
+        <p>This product uses the TMDB API but is not endorsed or certified by TMDB.</p>
+      </div>
+      <p className='copyrights'>All Right Reserved Nikos Gkatzogias 2022</p>
+    </footer>
   )
 }
