e24e7bbd99d40f93c6a95237d794bb92fbc391f9
NGkatzogias <nksgkatzogias@gmail.com>
2023-01-05 17:56:30 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Recent from "./components/Movies/Recent";
+import SearchResults from "./components/Movies/SearchResults";
+import SingleMovie from "./components/Movies/SingleMovie";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+
+function App() {
+  const [pageNumber, setPageNumber] = useState(1);
+  const [searchText, setSearchText] = useState("");
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 400) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  }, []);
+
+  const backToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <HashRouter>
+      <Header
+        setPageNumber={setPageNumber}
+        searchText={searchText}
+        setSearchText={setSearchText}
+      />
+      <div className="container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Recent pageNumber={pageNumber} setPageNumber={setPageNumber} />
+            }
+          />
+          <Route
+            path="SearchResults"
+            element={
+              <SearchResults
+                pageNumber={pageNumber}
+                setPageNumber={setPageNumber}
+                searchText={searchText}
+                setSearchText={setSearchText}
+              />
+            }
+          />
+          <Route path="SingleMovie/:id" element={<SingleMovie />} />
+        </Routes>
+      </div>
+      <Footer />
+      {showTopBtn && (
+        <button
+          className="back-to-top"
+          onClick={backToTop}
+          aria-label="back to top"
+        >
+          <FontAwesomeIcon icon={faAngleUp} />
+        </button>
+      )}
+    </HashRouter>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  HashRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import './index.css';
-// import App from './App';
-import Layout from './components/Layout';
-import Recent from './components/Movies/Recent';
-import SearchResults from './components/Movies/SearchResults';
-import SingleMovie from './components/Movies/SingleMovie';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <HashRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Recent />} />
-        <Route path="SearchResults" element={<SearchResults />} />
-        <Route path="SingleMovie/:id" element={<SingleMovie />} /> 
-      </Route>
-    </Routes>
-  </HashRouter>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
